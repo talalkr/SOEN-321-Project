2caad3d466180078804cccfd1696c6a9551607af
talalkr <talal.alrashid@gmail.com>
2019-03-28 21:20:52 -0400

    Attachment Implemented

--- autosend.py
+++ autosend.py
@@ -2,6 +2,8 @@ import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 #Setup Connection
 server = smtplib.SMTP('smtp.concordia.ca', 25)
@@ -14,9 +16,20 @@ subjects = ["", "Paypal Support - Subscription Renewal", "noreply", "Sign Up Tod
 #Content
 emptyMsg = ("")
 linkMsg = ("https://paypal-supportteam.myvnc.com/home/mostronix/248f4e93bcd0e09fb83f5641816dfa02OGJhM2I2OGM2NTBkYzZhNDVlZDFlMzYyNzJmZWI1YjE=/resolution/websc_login/?locale.x=en_")
-attachMsg = ("")
+attachMsg = "blahblah"
 benignMsg = ("Prosperous impression had conviction. \n\n For every delay death ask style. Me mean able my by in they. Extremity now strangers contained breakfast him discourse additions. Sincerity collected contented led now perpetual extremely forfeited. Perhaps far exposed age effects. Now distrusts you her delivered applauded affection out sincerity. As tolerably recommend shameless unfeeling he objection consisted. She although cheerful perceive screened throwing met not eat distance. Viewing hastily or written dearest elderly up weather it as. So direction so sweetness or extremity at daughters. Provided put unpacked now bu bringing. She exposed painted fifteen are noisier mistake led waiting. Surprise not wandered speedily husbands although yet end. Are court tiled cease young built fat one man taken. We highest ye friends is exposed equally in. Ignorant had too strictly followed. Astonished as travelling assistance or unreserved oh pianoforte ye. Five with seen put need tore add neat. Bringing it is he returned received raptures. ")
 phishMsg = ("https://autenticapp.cf/cx/")
+#Creating Attachment
+# open the file to be sent  
+filename = "signup.png"
+attachment = open("/home/t/t_alra/Downloads/Project.zip", "rb")
+# instance of MIMEBase 
+p = MIMEBase('application', 'octet-stream')
+# To change the payload into encoded form 
+p.set_payload((attachment).read())
+# encode into base64 
+encoders.encode_base64(p)
+p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
 #Body
 bodies = [emptyMsg, linkMsg, attachMsg, benignMsg, phishMsg]
 #Receivers
@@ -31,6 +44,9 @@ for rcvr in mailTo[0:]:
                 msg["To"] = rcvr
                 msg['Subject'] = subject
                 msg.attach(MIMEText(body, 'plain'))
+                # attach the instance 'p' to instance 'msg' 
+                if body is attachMsg:
+                   msg.attach(p)
                 text = msg.as_string()
                 server.sendmail(sender, rcvr, text)
 server.quit()
